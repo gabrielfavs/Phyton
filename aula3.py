7c870e196e90d34d1080d7a1036387ac0af31773
Gabriel Favalessa <favalessagabriel33@gmail.com>
2025-11-10 12:08:10 -0300

    Primeiro commit

---
+++ aula3.py
@@ -0,0 +1,27 @@
+"""
+DocString
+Python = Linguagem de promagração
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+print(1234)
+
+
+# Aspas simples
+print('Luiz Otávio')
+print('Luiz "Otávio"')
+
+
+# Aspas duplas
+print("Luiz Otávio")
+print("Luiz 'Otávio'")
+
+
+# Escape
+print("Luiz \"Otávio\"")
+
+      
+# r
+print(r"Luiz Otávio\"")
---
+++ aula4.py
@@ -0,0 +1,22 @@
+#Tipos int e float
+#int -> Número inteiro
+#O tipo int representa qualquer número
+# Positivo ou negativo. int sem sinal é considerado
+# Positivo.
+
+print(11) # int
+print(-11) # int
+print(0)
+
+# Float -> Número com ponto flutuante
+# O tipo float representa qualquer número
+# positivo ou negativo com ponto flutuante.
+# Float sem sinal é considera positivo.
+print(1.1, 10.11)
+print(0.0, -1.5)
+
+# A função type mostra o tipo que o Python
+# inferiu ao valor.
+print(type('Otávio'))
+print(type(0))
+print(type(1.1), type(-1.1), type(0.0))
